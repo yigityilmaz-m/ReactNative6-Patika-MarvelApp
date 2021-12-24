8d6b075309964523c2cde1e2357229529b2b84b5
umurislamoglu <74820307+umurislamoglu@users.noreply.github.com>
2021-12-24 15:29:35 +0300

    aligning color scheme with device default done

--- src/navigation/Navigation.js
+++ src/navigation/Navigation.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, DefaultTheme, DarkTheme} from '@react-navigation/native';
 import HeroStack from './HeroStack';
 import Favorites from '../pages/Favorites';
 import Settings from '../pages/Settings';
 import routes from './routes';
+import { useColorScheme } from 'react-native';
 
 const Tab = createBottomTabNavigator();
 
 export default function Navigation() {
+
+  const scheme = useColorScheme();
+
+
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={scheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Tab.Navigator initialRouteName={routes.HERO_STACK}>    
         <Tab.Screen name={routes.FAVORITES_PAGE} component={Favorites} />
         <Tab.Screen name={routes.HERO_STACK} component={HeroStack} />
